da454071d37be4e9955ce6bd9abc9c4fcc79e4a0
Zihui Deng <451809218@qq.com>
2026-07-07 10:37:56 +0800

    [fix][mds] Fixup compact panic issues.

--- src/mds/filesystem/store_operation.cc
+++ src/mds/filesystem/store_operation.cc
@@ -2614,15 +2614,17 @@ Status CompactChunkOperation::Run(TxnUPtr& txn) {
   CHECK(chunk.index() == chunk_index) << "chunk index not match.";
 
   const auto& slices = chunk.slices();
-  if (slices.at(param_.start_pos).id() != param_.start_slice_id) {
+  if (param_.start_pos >= slices.size() || slices.at(param_.start_pos).id() != param_.start_slice_id) {
     result_.chunk = chunk;
     return Status(pb::error::EILLEGAL_PARAMTETER,
-                  fmt::format("not match start slice id({})", slices.at(param_.start_pos).id()));
+                  fmt::format("not match start slice id({})",
+                              param_.start_pos >= slices.size() ? 0 : slices.at(param_.start_pos).id()));
   }
-  if (slices.at(param_.end_pos).id() != param_.end_slice_id) {
+  if (param_.end_pos >= slices.size() || slices.at(param_.end_pos).id() != param_.end_slice_id) {
     result_.chunk = chunk;
     return Status(pb::error::EILLEGAL_PARAMTETER,
-                  fmt::format("not match end slice id({})", slices.at(param_.end_pos).id()));
+                  fmt::format("not match end slice id({})",
+                              param_.end_pos >= slices.size() ? 0 : slices.at(param_.end_pos).id()));
   }
 
   // generate trash slice list
