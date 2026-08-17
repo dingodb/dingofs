2327b43e2f2de9fe36db4814aaa2eb04595d4615
wine93 <wine93.info@gmail.com>
2026-08-17 21:18:22 +0800

    feat(cache): add share-nothing tier

---
+++ src/cache/v2/tier/sharded.cc
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2026 dingodb.com, Inc. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cache/v2/tier/sharded.h"
+
+#include <glog/logging.h>
+
+#include <utility>
+
+#include "cache/v2/core/runtime/smp.h"
+
+namespace dingofs {
+namespace cache {
+namespace v2 {
+
+ShardedTierCache::ShardedTierCache(MDSClient* mds_client,
+                                   ObjectStorageUPtr storage)
+    : mds_client_(mds_client), storage_(std::move(storage)) {}
+
+ShardedTierCache::~ShardedTierCache() { Shutdown(); }
+
+Status ShardedTierCache::Start() {
+  CHECK(!running_) << "ShardedTierCache started twice";
+  CHECK_GT(ShardCount(), 0u) << "Runtime must be up first";
+
+  LOG(INFO) << "ShardedTierCache is starting...";
+
+  Status status = tiers_.StartOnAllShards([this](unsigned) {
+    return new TierCache(storage_.get(), mds_client_);
+  });
+  if (!status.ok()) {
+    tiers_.ShutdownOnAllShards();
+    return status;
+  }
+
+  running_ = true;
+  LOG(INFO) << "Successfully start ShardedTierCache{shards=" << ShardCount()
+            << "}";
+  return Status::OK();
+}
+
+void ShardedTierCache::Shutdown() {
+  if (!running_) {
+    return;
+  }
+
+  LOG(INFO) << "ShardedTierCache is shutting down...";
+
+  tiers_.ShutdownOnAllShards();
+
+  running_ = false;
+  LOG(INFO) << "Successfully shutdown ShardedTierCache";
+}
+
+Future<Status> ShardedTierCache::Put(BlockHandle handle, BufferViews block,
+                                     PutOption option) {
+  return InvokeOnOwner(handle, [=](TierCache& tier) {
+    return tier.Put(handle, block, option);
+  });
+}
+
+Future<Status> ShardedTierCache::Get(BlockHandle handle, uint64_t offset,
+                                     uint32_t length, char* buffer,
+                                     GetOption option) {
+  return InvokeOnOwner(handle, [=](TierCache& tier) {
+    return tier.Get(handle, offset, length, buffer, option);
+  });
+}
+
+Future<Status> ShardedTierCache::Prefetch(BlockHandle handle,
+                                          PrefetchOption option) {
+  return InvokeOnOwner(
+      handle, [=](TierCache& tier) { return tier.Prefetch(handle, option); });
+}
+
+Future<CacheStats> ShardedTierCache::GetStats() {
+  return tiers_.MapReduce(
+      CacheStats{}, [](TierCache& tier) { return tier.GetStats(); },
+      [](CacheStats& sum, CacheStats part) { sum.Merge(part); });
+}
+
+}  // namespace v2
+}  // namespace cache
+}  // namespace dingofs
---
+++ src/cache/v2/tier/sharded.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2026 dingodb.com, Inc. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DINGOFS_CACHE_V2_TIER_SHARDED_H_
+#define DINGOFS_CACHE_V2_TIER_SHARDED_H_
+
+#include <memory>
+#include <utility>
+
+#include "cache/v2/common/mds_client.h"
+#include "cache/v2/common/route.h"
+#include "cache/v2/core/runtime/sharded.h"
+#include "cache/v2/object/object.h"
+#include "cache/v2/tier/tier_cache.h"
+
+namespace dingofs {
+namespace cache {
+namespace v2 {
+
+class ShardedTierCache {
+ public:
+  ShardedTierCache(MDSClient* mds_client, ObjectStorageUPtr storage);
+  ~ShardedTierCache();
+
+  ShardedTierCache(const ShardedTierCache&) = delete;
+  ShardedTierCache& operator=(const ShardedTierCache&) = delete;
+
+  Status Start();
+  void Shutdown();
+
+  Future<Status> Put(BlockHandle handle, BufferViews block,
+                     PutOption option = {});
+  Future<Status> Get(BlockHandle handle, uint64_t offset, uint32_t length,
+                     char* buffer, GetOption option = {});
+  Future<Status> Prefetch(BlockHandle handle, PrefetchOption option = {});
+  Future<CacheStats> GetStats();
+
+ private:
+  template <typename Fn>
+  auto InvokeOnOwner(const BlockHandle& handle, Fn fn) {
+    return tiers_.InvokeOn(OwnerShard(handle), std::move(fn));
+  }
+
+  bool running_ = false;
+  MDSClient* mds_client_;
+  ObjectStorageUPtr storage_;
+  Sharded<TierCache> tiers_;
+};
+
+using ShardedTierCacheUPtr = std::unique_ptr<ShardedTierCache>;
+
+}  // namespace v2
+}  // namespace cache
+}  // namespace dingofs
+
+#endif  // DINGOFS_CACHE_V2_TIER_SHARDED_H_
